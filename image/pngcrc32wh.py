b4dc33f6832f3e1ca148e334b6c9e7d8d49bf56f
Ascetics <mingkun.ling@qq.com>
2022-09-12 12:54:56 +0800

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   49F5F.png_new.png
    	modified:   pngcrc32wh.py
    
     The first version of pngcrc32wh.py that can be used practically is released.

--- image/pngcrc32wh.py
+++ image/pngcrc32wh.py
@@ -25,17 +25,17 @@ def process_cmd_argv(argv):
     "\n",
     "usage: python pngcrc32wh.py -h\n",
     "\techo this help message.\n",
-    "usage: python pngcrc32wh.py -f <pngfile> -m <mode> -n <nrange>\n",
+    "usage: python pngcrc32wh.py -f <pngfile> -m <mode> -n <crack_range>\n",
     "\tcrack a png's width or height to satisfy the crc32 checksum.\n",
     "\t<pngfile> must be a png image file.\n",
     "\t<mode> can be: width, height.\n",
-    "\t<nrange> must be a valid positive integer.\n",
+    "\t<crack_range> must be a valid positive integer.\n",
     "\n",))
     mode = ("width", "height")    
     argv_dict = {}
 
     try:
-        opts, args = getopt.getopt(argv, "hf:m:n:", ["pngfile=","mode=","nrange="])
+        opts, args = getopt.getopt(argv, "hf:m:n:", ["pngfile=","mode=","crack_range="])
     except getopt.GetoptError:
         print(usage)
         sys.exit(2)
@@ -48,29 +48,60 @@ def process_cmd_argv(argv):
             argv_dict["pngfile"] = arg
         elif opt in ("-m", "--mode") and arg in mode:
             argv_dict["mode"] = arg
-        elif opt in("-n", "--nrange"):
+        elif opt in("-n", "--crack_range"):
             try:
-                nrange = int(arg)
-                argv_dict["nrange"] = nrange
+                crack_range = int(arg)
+                argv_dict["crack_range"] = crack_range
             except:
                 print(usage)
                 sys.exit(2)
 
-    if argv_dict.get("pngfile") and argv_dict.get("mode") and argv_dict.get("nrange"):
+    if argv_dict.get("pngfile") and argv_dict.get("mode") and argv_dict.get("crack_range"):
         return argv_dict
     else:
         print(usage)
         sys.exit(2)
 
-def png_crack_crc32wh(filename, mode):
+def png_crack_crc32wh(pngfile, mode, crack_range):
     '''
+    Read a png image file that can't be reading on linux platform, 
+    reporting Fatal error IHDR CRC error.
+    Brute force crack width or height to satisfy the crc32 checksum.
+    Output a cracked png file.
     '''
-    pass
+    with open(pngfile, "rb") as fin:
+        png_bytes = fin.read()
+        if "width" == mode:
+            wh_offset = 4
+        elif "height" == mode:
+            wh_offset = 8
+
+        # Brute force crack width or height to satisfy the crc32 checksum.
+        # Bytes can't be read or witten. You must use a bytearray.
+        data = bytearray(png_bytes[12:29])
+        crc32key = int.from_bytes(png_bytes[29:33], "big")
+        for n in range(crack_range):
+            substitute = bytearray(struct.pack(">I", n))
+            for i in range(4):
+                data[wh_offset+i] = substitute[i]
+            crc32result = zlib.crc32(data)
+            if crc32result == crc32key:
+                print("crack %s : " % mode, hex(n))
+
+                # Output a cracked png file.
+                png_bytearray = bytearray(png_bytes)
+                png_bytearray[12:29] = data
+                with open(pngfile+"_new.png", "wb") as fout:
+                    fout.write(png_bytearray)
+                break
+
+        
+
 
 def main(argv):
     argv_dict = process_cmd_argv(argv)
-    pngfile, mode, nrange = argv_dict['pngfile'], argv_dict['mode'], argv_dict['nrange']
-    print(pngfile, mode, nrange)
+    pngfile, mode, crack_range = argv_dict.get("pngfile"), argv_dict.get("mode"), argv_dict.get("crack_range")
+    png_crack_crc32wh(pngfile, mode, crack_range)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
