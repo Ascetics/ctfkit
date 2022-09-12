87451f666f3175037f40176823ce0f99ff58d61b
Ascetics <mingkun.ling@qq.com>
2022-09-12 19:19:03 +0800

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
           modified:   cipher.py
    
    cipher.py holds base 16,32,64,85,58,91,92,62 cipher.

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -3,136 +3,148 @@ import base58
 import base62
 import base91
 import py3base92 as base92
+import base36
 import hashlib
 
 
 class Cipher():
-    @classmethod
-    def encode_string_base16(cls, s):
+    @staticmethod
+    def encode_string_base16(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:            
             return base64.b16encode(s_bytes).decode(encoding="utf-8")
         except:
             return None
     
-    @classmethod
-    def decode_string_base16(cls, s):
+    @staticmethod
+    def decode_string_base16(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:            
             return base64.b16decode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base32(cls, s):
+    @staticmethod
+    def encode_string_base32(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:
             return base64.b32encode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def decode_string_base32(cls, s):
+    @staticmethod
+    def decode_string_base32(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:
             return base64.b32decode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base64(cls, s):
+    @staticmethod
+    def encode_string_base64(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:            
             return base64.b64encode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def decode_string_base64(cls, s):
+    @staticmethod
+    def decode_string_base64(s):
         s_bytes = bytes(s, encoding="utf-8")      
         try:            
             return base64.b64decode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base85(cls, s):
+    @staticmethod
+    def encode_string_base85(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:
             return base64.b85encode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def decode_string_base85(cls, s):
+    @staticmethod
+    def decode_string_base85(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:
             return base64.b85decode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base58(cls, s):
+    @staticmethod
+    def encode_string_base58(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:            
             return base58.b58encode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def decode_string_base58(cls, s):
+    @staticmethod
+    def decode_string_base58(s):
         s_bytes = bytes(s, encoding="utf-8")      
         try:            
             return base58.b58decode(s_bytes).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base91(cls, s):
+    @staticmethod
+    def encode_string_base91(s):
         s_bytes = bytearray(s, encoding="utf-8")
         try:
             return base91.encode(s_bytes)
         except:
             return None
 
-    @classmethod
-    def decode_string_base91(cls, s):
+    @staticmethod
+    def decode_string_base91(s):
         try:
             return base91.decode(s).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_base92(cls, s):
+    @staticmethod
+    def encode_string_base92(s):
         try:
             return base92.encode(s)
         except:
             return None
 
-    @classmethod
-    def decode_string_base92(cls, s):
+    @staticmethod
+    def decode_string_base92(s):
         try:
             return base92.decode(s)
         except:
             return None
 
-    @classmethod
-    def encode_string_base62(cls, s):
+    @staticmethod
+    def encode_string_base62(s):
         s_bytes = bytes(s, encoding="utf-8")
         try:
             return base62.encodebytes(s_bytes)
         except:
             return None
 
-    @classmethod
-    def decode_string_base62(cls, s):
+    @staticmethod
+    def decode_string_base62(s):
         try:
             return base62.decodebytes(s).decode(encoding="utf-8")
         except:
             return None
 
-    @classmethod
-    def encode_string_md5(cls, s):
+    @staticmethod
+    def decode_string_base36(s):
+        '''
+        bin, oct, dec, hex, base36.
+        base36 require a string and return an integer.
+        '''
+        try:
+            return base36.loads(s)
+        except:
+            return None
+
+    @staticmethod
+    def encode_string_md5(s):
         try:
             s_bytes = bytes(s, encoding="utf-8")
             md5hash = hashlib.md5(s_bytes)
@@ -141,18 +153,18 @@ class Cipher():
         except:
             return None
 
-    @classmethod
-    def decode_string_md5(cls, s):
+    @staticmethod
+    def decode_string_md5(s):
         print("to be realized")
         pass
         
-    @classmethod
-    def encode_string_aes(cls, s):
+    @staticmethod
+    def encode_string_aes(s):
         print("to be realized")
         pass
 
-    @classmethod
-    def decode_string_aes(cls, s):
+    @staticmethod
+    def decode_string_aes(s):
         print("to be realized")
         pass
 
@@ -198,6 +210,7 @@ def test(s="Do you think the key word is welcome?"):
     print("base62: %s" % result)
     result = Cipher.decode_string_base62(result)
     print("base62: %s" % result)
+
     
 def base_family(s):
     result = Cipher.decode_string_base16(s)
@@ -222,7 +235,7 @@ def main():
     # base_family(s="StV1DL6CwTryKyV")                        # test for base58, you should see "base58: hello world"
     # base_family(s="fPNKd")                                  # test for base91, you should see "base91: test"    
     # base_family(s="8D9Kc)=/2$WzeFui#G9Km+<{VT2u9MZil}[A")   # test for base91, you should see "base91: May a moody baby doom a yam?\n"
-    # base_family(s="Jw_@V")                                    # test for base92, you should see "base92: test"
+    # base_family(s="Jw_@V")                                  # test for base92, you should see "base92: test"
     
     # base_family(s="U2FsdGVkX1+WTSHujcCjvHj/gcwL0C7u37XtW4idGcpci3H913I=") # test for nothing
 
