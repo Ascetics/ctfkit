f45afca774ef71f6160f16b874065519deef7db5
Ascetics <mingkun.ling@qq.com>
2022-09-12 16:57:08 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   base91.py
    	modified:   cipher.py
    
     cipher holds base 16, 32, 64, 85, 58, 91 cipher.

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -1,5 +1,6 @@
 import base64
 import base58
+import base91
 import hashlib
 
 
@@ -65,6 +66,26 @@ class Cipher():
             return None
 
     @classmethod
+    def encode_string_base85(cls, s):
+        s_bytes = bytes(s, encoding="utf-8")
+        try:
+            e_bytes = base64.b85encode(s_bytes)
+            e_str = str(e_bytes, encoding="utf-8")
+            return e_str
+        except:
+            return None
+
+    @classmethod
+    def decode_string_base85(cls, s):
+        s_bytes = bytes(s, encoding="utf-8")
+        try:
+            d_bytes = base64.b85decode(s_bytes)
+            d_str = str(d_bytes, encoding="utf-8")
+            return d_str
+        except:
+            return None
+
+    @classmethod
     def encode_string_base58(cls, s):
         s_bytes = bytes(s, encoding="utf-8")
         try:            
@@ -81,6 +102,23 @@ class Cipher():
             d_bytes = base58.b58decode(s_bytes)
             d_str = str(d_bytes, encoding="utf-8")
             return d_str
+        except:
+            return None
+
+    @classmethod
+    def encode_string_base91(cls, s):
+        s_bytes = bytearray(s, encoding="utf-8")
+        try:
+            e_str = base91.encode(s_bytes)
+            return e_str
+        except:
+            return None
+
+    @classmethod
+    def decode_string_base91(cls, s):
+        try:
+            d_bytearray = base91.decode(s)
+            return d_bytearray.decode()
         except:
             return None
 
@@ -109,25 +147,7 @@ class Cipher():
         print("to be realized")
         pass
 
-    @classmethod
-    def encode_string_base85(cls, s):
-        s_bytes = bytes(s, encoding="utf-8")
-        try:
-            e_bytes = base64.b85encode(s_bytes)
-            e_str = str(e_bytes, encoding="utf-8")
-            return e_str
-        except:
-            return None
-
-    @classmethod
-    def decode_string_base85(cls, s):
-        s_bytes = bytes(s, encoding="utf-8")
-        try:
-            d_bytes = base64.b85decode(s_bytes)
-            d_str = str(d_bytes, encoding="utf-8")
-            return d_str
-        except:
-            return None
+   
 
 def test(s="Do you think the key word is welcome?"):
     result = Cipher.encode_string_base16(s)
@@ -154,6 +174,11 @@ def test(s="Do you think the key word is welcome?"):
     print("base85: %s" % result)
     result = Cipher.decode_string_base85(result)
     print("base85: %s" % result)
+
+    result = Cipher.encode_string_base91(s)
+    print("base91: %s" % result)
+    result = Cipher.decode_string_base91(result)
+    print("base91: %s" % result)
     
 def base_family(s):
     result = Cipher.decode_string_base16(s)
@@ -166,11 +191,16 @@ def base_family(s):
     print("base64: %s" % result)
     result = Cipher.decode_string_base85(s)
     print("base85: %s" % result)
+    result = Cipher.decode_string_base91(s)
+    print("base91: %s" % result)
     pass
 
 def main():
-    test()
-    base_family(s="U2FsdGVkX1+WTSHujcCjvHj/gcwL0C7u37XtW4idGcpci3H913I=")
+    # test()
+    base_family(s="StV1DL6CwTryKyV")                        # test for base58, you should see "hello world"
+    base_family(s="fPNKd")                                  # test for base91, you should see "test"    
+    base_family(s="8D9Kc)=/2$WzeFui#G9Km+<{VT2u9MZil}[A")   # test for base91, you should see "May a moody baby doom a yam?\n"
+    base_family(s="U2FsdGVkX1+WTSHujcCjvHj/gcwL0C7u37XtW4idGcpci3H913I=") # test for nothing
 
 if __name__ == '__main__':
     main()
