f823f24d8c27300630e622f4129cd450dc9d41e0
Ascetics <mingkun.ling@qq.com>
2022-11-29 19:58:12 +0800

    cipher 参数为字节流

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -9,132 +9,119 @@ import hashlib
 
 class Cipher():
     @staticmethod
-    def encode_string_base16(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base16(s_bytes):
         try:
-            return base64.b16encode(s_bytes).decode(encoding="utf-8")
+            return base64.b16encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base16(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def dec_base16(s_bytes):
         try:
-            return base64.b16decode(s_bytes).decode(encoding="utf-8")
+            return base64.b16decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base32(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base32(s_bytes):
         try:
-            return base64.b32encode(s_bytes).decode(encoding="utf-8")
+            return base64.b32encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base32(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def dec_base32(s_bytes):
         try:
-            return base64.b32decode(s_bytes).decode(encoding="utf-8")
+            return base64.b32decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base64(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base64(s_bytes):
         try:
-            return base64.b64encode(s_bytes).decode(encoding="utf-8")
+            return base64.b64encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base64(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def dec_base64(s_bytes):
         try:
-            return base64.b64decode(s_bytes).decode(encoding="utf-8")
+            return base64.b64decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base85(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base85(s_bytes):
         try:
-            return base64.b85encode(s_bytes).decode(encoding="utf-8")
+            return base64.b85encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base85(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def dec_base85(s_bytes):
         try:
-            return base64.b85decode(s_bytes).decode(encoding="utf-8")
+            return base64.b85decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base58(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base58(s_bytes):
         try:
-            return base58.b58encode(s_bytes).decode(encoding="utf-8")
+            return base58.b58encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base58(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def dec_base58(s_bytes):
         try:
-            return base58.b58decode(s_bytes).decode(encoding="utf-8")
+            return base58.b58decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base91(s):
-        s_bytes = bytearray(s, encoding="utf-8")
+    def enc_base91(s_bytes):
         try:
             return base91.encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base91(s):
+    def dec_base91(s_bytes):
         try:
-            return base91.decode(s).decode(encoding="utf-8")
+            return base91.decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base92(s):
+    def enc_base92(s_bytes):
         try:
-            base92.b92encode(s)
-            return base92.encode(s)
+            return base92.encode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base92(s):
+    def dec_base92(s_bytes):
         try:
-            return base92.decode(s)
+            return base92.decode(s_bytes)
         except:
             return None
 
     @staticmethod
-    def encode_string_base62(s):
-        s_bytes = bytes(s, encoding="utf-8")
+    def enc_base62(s_bytes):
         try:
             return base62.encodebytes(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base62(s):
+    def dec_base62(s_bytes):
         try:
-            return base62.decodebytes(s).decode(encoding="utf-8")
+            return base62.decodebytes(s_bytes)
         except:
             return None
 
     @staticmethod
-    def decode_string_base36(s):
+    def dec_base36(s):
         '''
         bin, oct, dec, hex, base36.
         base36 require a string and return an integer.
@@ -154,85 +141,54 @@ class Cipher():
         except:
             return None
 
-    @staticmethod
-    def decode_string_md5(s):
-        print("to be realized")
-        pass
 
-    @staticmethod
-    def encode_string_aes(s):
-        print("to be realized")
-        pass
-
-    @staticmethod
-    def decode_string_aes(s):
-        print("to be realized")
-        pass
-
-
-def tt(s="Do you think the key word is welcome?"):
-    result = Cipher.encode_string_base16(s)
+def base_family(s="Do you think the key word is welcome?"):
+    s = s.encode('utf-8')
+    result = Cipher.enc_base16(s)
     print("base16: %s" % result)
-    result = Cipher.decode_string_base16(result)
+    result = Cipher.dec_base16(result)
     print("base16: %s" % result)
 
-    result = Cipher.encode_string_base32(s)
+    result = Cipher.enc_base32(s)
     print("base32: %s" % result)
-    result = Cipher.decode_string_base32(result)
+    result = Cipher.dec_base32(result)
     print("base32: %s" % result)
 
-    result = Cipher.encode_string_base64(s)
+    result = Cipher.enc_base64(s)
     print("base64: %s" % result)
-    result = Cipher.decode_string_base64(result)
+    result = Cipher.dec_base64(result)
     print("base64: %s" % result)
 
-    result = Cipher.encode_string_base85(s)
+    result = Cipher.enc_base85(s)
     print("base85: %s" % result)
-    result = Cipher.decode_string_base85(result)
+    result = Cipher.dec_base85(result)
     print("base85: %s" % result)
 
-    result = Cipher.encode_string_base58(s)
+    result = Cipher.enc_base58(s)
     print("base58: %s" % result)
-    result = Cipher.decode_string_base58(result)
+    result = Cipher.dec_base58(result)
     print("base58: %s" % result)
 
-    result = Cipher.encode_string_base91(s)
+    result = Cipher.enc_base91(s)
     print("base91: %s" % result)
-    result = Cipher.decode_string_base91(result)
+    result = Cipher.dec_base91(result)
     print("base91: %s" % result)
 
-    result = Cipher.encode_string_base92(s)
+    result = Cipher.enc_base92(s)
     print("base92: %s" % result)
-    result = Cipher.decode_string_base92(result)
+    result = Cipher.dec_base92(result)
     print("base92: %s" % result)
 
-    result = Cipher.encode_string_base62(s)
+    result = Cipher.enc_base62(s)
     print("base62: %s" % result)
-    result = Cipher.decode_string_base62(result)
+    result = Cipher.dec_base62(result)
     print("base62: %s" % result)
 
 
-def base_family(s):
-    result = Cipher.decode_string_base16(s)
-    print("base16: %s" % result)
-    result = Cipher.decode_string_base32(s)
-    print("base32: %s" % result)
-    result = Cipher.decode_string_base58(s)
-    print("base58: %s" % result)
-    result = Cipher.decode_string_base64(s)
-    print("base64: %s" % result)
-    result = Cipher.decode_string_base85(s)
-    print("base85: %s" % result)
-    result = Cipher.decode_string_base91(s)
-    print("base91: %s" % result)
-    result = Cipher.decode_string_base92(s)
-    print("base92: %s" % result)
-    result = Cipher.decode_string_base62(s)
-    print("base62: %s" % result)
 
 
 def main():
-    tt()
+    base_family()
     # base_family(s="StV1DL6CwTryKyV")                        # test for base58, you should see "base58: hello world"
     # base_family(s="fPNKd")                                  # test for base91, you should see "base91: test"    
     # base_family(s="8D9Kc)=/2$WzeFui#G9Km+<{VT2u9MZil}[A")   # test for base91, you should see "base91: May a moody baby doom a yam?\n"
