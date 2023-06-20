7d79dc0f637f777a05e97bc3f1307e6bc6f2384f
Ascetics <mingkun.ling@qq.com>
2023-06-21 00:26:34 +0800

    三维迷宫，深度优先算法，迷宫是二维字符串列表

--- reverse/maze3d.py
+++ reverse/maze3d.py
@@ -1,6 +1,6 @@
 # -*- coding:utf8 -*-
 """
-三维迷宫，深度优先算法
+三维迷宫，深度优先算法，迷宫是二维字符串列表
 从@经过*走到&，键盘ad控制z的-+，键盘ws控制y的-+，键盘oi控制x的-+
 二维数组为x坐标，每个二维数组 行是y坐标，列是z坐标，定义坐标为（x,y,z）
 z[0]
@@ -59,6 +59,9 @@ z[6]
 *****##
 #####&#
 #####**
+
+操作路径：
+idwdisidwwaaawaasossaisddddossoaawawaowwwwddodssddwoowaaasiaissdsdddidwoosssiiiiiaw
 """
 START = '@'  # 起点
 FINISH = '&'  # 终点
@@ -126,15 +129,20 @@ VISIT = [[[0 for z in range(MAZE_Z)] for y in range(MAZE_Y)] for x in
 X_MINUS, X_PLUS = 'o', 'i'
 Y_MINUS, Y_PLUS = 'w', 's'
 Z_MINUS, Z_PLUS = 'a', 'd'
-FOUND = False  # 到达终点
 RESULT = []  # 路径操作
 
 
 def help3d(x, y, z):
     global START, FINISH, WALL, PASS, MAZE, MAZE_Z, MAZE_Y, MAZE_X, VISIT
-    global X_MINUS, X_PLUS, Y_MINUS, Y_PLUS, Z_MINUS, Z_PLUS, FOUND, RESULT
+    global X_MINUS, X_PLUS, Y_MINUS, Y_PLUS, Z_MINUS, Z_PLUS, RESULT
+
     VISIT[x][y][z] = 1
-    for step in {X_MINUS, X_PLUS, Y_MINUS, Y_PLUS, Z_MINUS, Z_PLUS}:
+
+    if MAZE[x][y][z] == FINISH:
+        print(''.join(RESULT))
+        return
+
+    for step in (X_MINUS, X_PLUS, Y_MINUS, Y_PLUS, Z_MINUS, Z_PLUS,):
         newx, newy, newz = x, y, z
         if step == X_MINUS:
             newx -= 1
@@ -149,24 +157,19 @@ def help3d(x, y, z):
         elif step == Z_PLUS:
             newz += 1
 
-        if (0 <= newx < MAZE_X and 0 <= newy < MAZE_Y and 0 <= newz < MAZE_Z) \
-            and \
-            (MAZE[newx][newy][newz] == PASS or
-             MAZE[newx][newy][newz] == FINISH) \
-            and VISIT[newx][newy][newz] == 0:
-            if MAZE[newx][newy][newz] == PASS:
-                help3d(newx, newy, newz)
-                if FOUND:
-                    RESULT.append(step)  # 到达终点时才记录路径
-            elif MAZE[newx][newy][newz] == FINISH:
-                FOUND = True
-                RESULT.append(step)
-                print('Bingo!')
-                return
+        if (
+            0 <= newx < MAZE_X and 0 <= newy < MAZE_Y and 0 <= newz < MAZE_Z
+            and (
+                MAZE[newx][newy][newz] == PASS
+                or MAZE[newx][newy][newz] == FINISH
+            )
+            and VISIT[newx][newy][newz] == 0
+        ):
+            RESULT.append(step)
+            help3d(newx, newy, newz)
+            RESULT.pop(-1)
+            VISIT[newx][newy][newz] = 0
 
 
 if __name__ == '__main__':
     help3d(3, 3, 3)
-    RESULT.reverse()  # 递归记录，需要反向
-    for x in RESULT:
-        print(x, end='')
