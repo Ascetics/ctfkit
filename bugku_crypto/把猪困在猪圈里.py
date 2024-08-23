58dcc43b26e222d2a31281b741d3a4910f1654cc
Ascetics <mingkun.ling@qq.com>
2024-08-23 22:24:48 +0800

    prepare for 20240909

--- bugku_crypto/把猪困在猪圈里.py
+++ bugku_crypto/把猪困在猪圈里.py
@@ -6,11 +6,11 @@ f1 = open('把猪困在猪圈里.txt', 'r', encoding='utf-8')
 c = f1.read()
 f1.close()
 
-# base64解码，查看16个字节有没有magic
+# base64解码，查看16个字节有没有magic，发现是JPG图片
 m = base64.b64decode(c)
 print(m[0:16])
 
 # 输出到二进制文件
-f = open('把猪困在猪圈里.bin', 'wb')
+f = open('把猪困在猪圈里.jpg', 'wb')
 f.write(m)
 f.close()
