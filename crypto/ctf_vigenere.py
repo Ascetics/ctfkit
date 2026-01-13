1758d4a1d6f1dfb7fd876570ac334bf32303a49e
mingkun.ling <mingkun.ling@qq.com>
2026-01-13 22:44:12 +0800

    维吉尼亚密码Vigenere

--- crypto/ctf_vigenere.py
+++ crypto/ctf_vigenere.py
@@ -3,62 +3,54 @@
 Vigenere cipher is an advanced Caesar cipher.
 """
 
-TAB = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-       'BCDEFGHIJKLMNOPQRSTUVWXYZA',
-       'CDEFGHIJKLMNOPQRSTUVWXYZAB',
-       'DEFGHIJKLMNOPQRSTUVWXYZABC',
-       'EFGHIJKLMNOPQRSTUVWXYZABCD',
-       'FGHIJKLMNOPQRSTUVWXYZABCDE',
-       'GHIJKLMNOPQRSTUVWXYZABCDEF',
-       'HIJKLMNOPQRSTUVWXYZABCDEFG',
-       'IJKLMNOPQRSTUVWXYZABCDEFGH',
-       'JKLMNOPQRSTUVWXYZABCDEFGHI',
-       'KLMNOPQRSTUVWXYZABCDEFGHIJ',
-       'LMNOPQRSTUVWXYZABCDEFGHIJK',
-       'MNOPQRSTUVWXYZABCDEFGHIJKL',
-       'NOPQRSTUVWXYZABCDEFGHIJKLM',
-       'OPQRSTUVWXYZABCDEFGHIJKLMN',
-       'PQRSTUVWXYZABCDEFGHIJKLMNO',
-       'QRSTUVWXYZABCDEFGHIJKLMNOP',
-       'RSTUVWXYZABCDEFGHIJKLMNOPQ',
-       'STUVWXYZABCDEFGHIJKLMNOPQR',
-       'TUVWXYZABCDEFGHIJKLMNOPQRS',
-       'UVWXYZABCDEFGHIJKLMNOPQRST',
-       'VWXYZABCDEFGHIJKLMNOPQRSTU',
-       'WXYZABCDEFGHIJKLMNOPQRSTUV',
-       'XYZABCDEFGHIJKLMNOPQRSTUVW',
-       'YZABCDEFGHIJKLMNOPQRSTUVWX',
-       'ZABCDEFGHIJKLMNOPQRSTUVWXY', ]
+
+def check_key(key):
+    for k in key:
+        if not k.islower() and not k.isupper():
+            print('The key must consist only of letters')
 
 
 def encode(string, key):
-    ki = 0
-    c_text = ''
+    check_key(key)
+    si, ki, c_text = 0, 0, ''
+    while si < len(string):
+        s, k = string[si], key[ki % len(key)]
+        if not s.islower() and not s.isupper():
+            c_text += s
+            si += 1
+            continue
 
-    for i in string:
-        if i.islower() or i.isupper():
-            row = ord(i.upper()) - ord('A')
-            col = ord(key[ki % len(key)].upper()) - ord('A')
-            c_text += TAB[row][col].lower() if i.islower() else TAB[row][col]
-            ki += 1
-        else:
-            c_text += i
+        c, offset = ord(s), ord(k.upper()) - ord('A')
+        if s.isupper():
+            c = (c - ord('A') + offset) % 26 + ord('A')
+        elif s.islower():
+            c = (c - ord('a') + offset) % 26 + ord('a')
+        c_text += chr(c)
+        si += 1
+        ki += 1
+
     return c_text
 
 
 def decode(string, key):
-    ki = 0
-    m_text = ''
+    check_key(key)
+    si, ki, m_text = 0, 0, ''
+    while si < len(string):
+        s, k = string[si], key[ki % len(key)]
+        if not s.islower() and not s.isupper():
+            m_text += s
+            si += 1
+            continue
 
-    for i in string:
-        if i.islower() or i.isupper():
-            for line in TAB:
-                if line[ord(key[ki % len(key)].upper()) - ord('A')] == i.upper():
-                    m_text += line[0].lower() if i.islower() else line[0]
-                    ki += 1
-                    break
-        else:
-            m_text += i
+        c, offset = ord(s), ord(k.upper()) - ord('A')
+        if s.isupper():
+            c = (c - ord('A') + 26 - offset) % 26 + ord('A')
+        elif s.islower():
+            c = (c - ord('a') + 26 - offset) % 26 + ord('a')
+        m_text += chr(c)
+        si += 1
+        ki += 1
+
     return m_text
 
 
